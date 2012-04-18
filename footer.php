322c10d887727432dbaf92a1dacdeb30465b2025
Abdullah Adnan <mail.adnan.ku@gmail.com>
2012-04-18 18:40:43 +0700

    Little Changes
    footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package saayana
- * @since saayana 0.1
- */
 ?>
 
 	</div><!-- #main -->
@@ -16,7 +8,7 @@
 			<?php do_action( 'saayana_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'saayana' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'saayana' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'saayana' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'saayana' ), 'saayana', '<a href="http://adnanomatic.com/" rel="designer">Adnanomatic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'saayana' ), 'Saayana', '<a href="http://adnanomatic.com/" rel="designer">Adnanomatic</a>' ); ?>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
